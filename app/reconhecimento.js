5a95519e1840f22cf1bf12b698b3c629dba34ef1
PomboObeso <matheus.gomes201@gmail.com>
2022-12-28 20:57:00 -0300

    consumindo a api web speech

---
+++ app/reconhecimento.js
@@ -0,0 +1,24 @@
+const elementChute = document.getElementById('chute')
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.lang='pt-Br';
+recognition.start();
+
+
+recognition.addEventListener('result', onSpeak);
+
+
+function onSpeak(event) {
+    let chute = event.results[0][0].transcript;
+    exibirChute(chute);
+}
+
+function exibirChute(chute){
+    elementChute.innerHTML = `
+        <div>Você disse:</div>
+        <span class="box">${chute}</span>
+    `
+}
